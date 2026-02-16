7c0a3ca772741bbd1a22b1adcd384ac2e1f84fe7
Victoria I Opara <vopara8@gmail.com>
2026-02-16 02:27:43 +0000

    web_scraper

---
+++ scrapers/scrape.py
@@ -0,0 +1,6 @@
+import requests
+
+url = 'https://www.mbp.state.md.us/sanctions.aspx'
+response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'})
+html = response.content
+print(html)
