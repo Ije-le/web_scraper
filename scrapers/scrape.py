2eac74cbaf3b10fa8193899e63bb769ce6af719e
Victoria I Opara <vopara8@gmail.com>
2026-02-21 23:57:24 +0000

    scraper

--- scrapers/scrape.py
+++ scrapers/scrape.py
@@ -14,8 +14,11 @@ list_of_rows = []
 for row in table.find_all('tr'):
     list_of_cells = []
     for cell in row.find_all('td'):
-        text = ' '.join(cell.text.split())
-        list_of_cells.append(text)
+        if cell.find('a'):
+            list_of_cells.append(cell.find('a')['href'])
+        else:
+            text = ' '.join(cell.text.split())
+            list_of_cells.append(text)
     list_of_rows.append(list_of_cells)
 
 outfile = open("alerts.csv", "w")
@@ -23,3 +26,5 @@ writer = csv.writer(outfile)
 writer.writerow(["url", "name", "type", "date"])
 writer.writerows(list_of_rows)
 
+# Note: the code extracts the links from the table and brings it into the csv file, but the links did not work. It is something with how the links are being extracted. This pagea has been modified, so there is no need to add https://www.mbps... again. I deleted the "https://www.mbp.state.md.us" from the if statement, and now the links work.  
+# Follow up on when to complete year selector part of tutorial.
